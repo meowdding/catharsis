8ac3f9857bfa5932b9432e3cefb16897703d2aee
Mona <59416038+meowora@users.noreply.github.com>
2025-10-28 13:12:16 +0100

    feat(docs): bounding box and position schemas

---
+++ docs/components/schemas/bounding_box.vue
@@ -0,0 +1,58 @@
+<script setup>
+import TypeIcon from "../type-icon.vue";
+import Position from "./position.vue";
+import TreeView from "../tree-view.vue";
+
+const props = defineProps({
+    positionType: {
+        type: String,
+        required: true
+    },
+    customText: {
+        type: String,
+        required: false
+    }
+})
+</script>
+<template>
+    <TreeView collapsable>
+        <template #title>
+            <TypeIcon/>
+            &nbsp;
+            <span>A bounding box marking a region in 3d space</span>
+        </template>
+        <ul>
+            <li><span><TypeIcon type="array"/> An array with two positions</span>
+                <ul>
+                    <li>
+                        <Position position-type="int"/>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <span><TypeIcon type="array"/> An array with 6 ints</span>
+                <ul>
+                    <li>
+                        <span><TypeIcon type="int"/> The coordinates in order <code>minX</code>, <code>minY</code>, <code>minZ</code>, <code>maxX</code>, <code>maxY</code>, <code>maxZ</code></span>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <span><TypeIcon type="object"/> An object with min and max</span>
+                <ul>
+                    <li>
+
+                        <Position position-type="int" custom-text>
+                            <b>min</b>: The min position
+                        </Position>
+                    </li>
+                    <li>
+                        <Position position-type="int" custom-text>
+                            <b>max</b>: The max position
+                        </Position>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </TreeView>
+</template>
--- docs/components/schemas/position.vue
+++ docs/components/schemas/position.vue
@@ -1,23 +1,58 @@
 <script setup>
 import TypeIcon from "../type-icon.vue";
+import TreeView from "../tree-view.vue";
 
 const props = defineProps({
     positionType: {
-        type: "float" | "int" | "both",
+        type: String,
         required: true
     },
     customText: {
-        type: String,
+        type: Boolean,
         required: false
     }
 })
 </script>
 <template>
-    <TypeIcon/>
-    <template v-if="customText === undefined">
-        <span v-if="positionType === 'both'">A position of any precision</span>
-        <span v-else-if="positionType === 'int'">A block position of ints</span>
-        <span v-else>A 3d position of floats</span>
-    </template>
-    <span v-else :content="customText"></span>
+    <TreeView collapsable>
+        <template #title>
+            <TypeIcon :type="positionType"/>
+            <span>&nbsp;</span>
+            <template v-if="!customText">
+                <span v-if="positionType === 'float'">A position with float precision</span>
+                <span v-else-if="positionType === 'int'">A position with int precision</span>
+                <span v-else>A position with any precision</span>
+            </template>
+            <span v-else><slot/></span>
+        </template>
+        <ul>
+            <li>
+                <TypeIcon type="array"/>
+                An array of three <span v-if="positionType === 'float'">floats</span><span v-else-if="positionType==='int'">ints</span><span v-else>numbers</span></li>
+            <li>
+                <TypeIcon type="string"/>
+                A string of coordinates parts formatted as <code>x:y:z</code></li>
+            <li>
+                <TypeIcon type="object"/>
+                An object with the coordinate values
+                <ul>
+                    <li>
+                        <TypeIcon v-if="positionType !== 'float'" type="int"/>
+                        <TypeIcon v-if="positionType !== 'int'" type="float"/>
+                        &nbsp;<b>x</b>: The x part of the coordinate
+                    </li>
+                    <li>
+                        <TypeIcon v-if="positionType !== 'float'" type="int"/>
+                        <TypeIcon v-if="positionType !== 'int'" type="float"/>
+                        &nbsp;<b>y</b>: The y part of the coordinate
+                    </li>
+                    <li>
+                        <TypeIcon v-if="positionType !== 'float'" type="int"/>
+                        <TypeIcon v-if="positionType !== 'int'" type="float"/>
+                        &nbsp;<b>z</b>: The z part of the coordinate
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </TreeView>
 </template>

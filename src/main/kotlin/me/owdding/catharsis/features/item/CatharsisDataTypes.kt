d18ccba27b9fe416b6485e3e230453cb6806a3d9
Gravy Boat <gravy@thatgravyboat.tech>
2026-02-04 22:23:14 -0330

    fix: dont use cleanname inside of data types

--- src/main/kotlin/me/owdding/catharsis/features/item/CatharsisDataTypes.kt
+++ src/main/kotlin/me/owdding/catharsis/features/item/CatharsisDataTypes.kt
@@ -3,19 +3,19 @@ package me.owdding.catharsis.features.item
 import me.owdding.catharsis.PreLoadModule
 import tech.thatgravyboat.skyblockapi.api.datatype.DataType
 import tech.thatgravyboat.skyblockapi.api.datatype.defaults.GenericDataTypes
-import tech.thatgravyboat.skyblockapi.utils.extentions.cleanName
+import tech.thatgravyboat.skyblockapi.utils.text.TextProperties.stripped
 
 @PreLoadModule
 object CatharsisDataTypes {
 
     val HAS_SKIN_FALLBACK = DataType.of("has_skin_fallback") {
         if (GenericDataTypes.HELMET_SKIN.factory(it) != null) true
-        else it.cleanName.endsWith("✦")
+        else it.hoverName.stripped.endsWith("✦")
     }
 
     val HAS_DYE_FALLBACK = DataType.of("has_dye_fallback") {
         if (GenericDataTypes.APPLIED_DYE.factory(it) != null) true
-        else it.cleanName.startsWith("✿").takeIf { name -> name }
+        else it.hoverName.stripped.startsWith("✿").takeIf { name -> name }
     }
 
 }

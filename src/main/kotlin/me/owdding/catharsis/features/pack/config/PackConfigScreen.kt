7091a47fbdf0dd368c9958decece9cde058507a6
Mona <git@mona.cat>
2026-04-20 01:17:54 +0200

    fix: not rendering config screen

--- src/main/kotlin/me/owdding/catharsis/features/pack/config/PackConfigScreen.kt
+++ src/main/kotlin/me/owdding/catharsis/features/pack/config/PackConfigScreen.kt
@@ -242,6 +242,7 @@ class PackConfigScreenTab(val title: Component, val contents: Layout) : Tab {
     override fun getTabExtraNarration(): Component = Component.empty()
     override fun visitChildren(consumer: Consumer<AbstractWidget>) = layout.visitWidgets(consumer)
     override fun doLayout(rectangle: ScreenRectangle) {
+        this.contents.arrangeElements()
         this.contents.visitChildren { (it as? Layout)?.arrangeElements() }
         this.layout.setMaxHeight(rectangle.height - 20)
         this.layout.arrangeElements()

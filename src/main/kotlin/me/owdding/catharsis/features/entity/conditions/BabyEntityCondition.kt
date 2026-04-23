b9d02a5fead0281b54ad047352d1c85f45aae673
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2026-04-23 10:20:06 +0200

    refactor: improve baby matching code
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/me/owdding/catharsis/features/entity/conditions/BabyEntityCondition.kt
+++ src/main/kotlin/me/owdding/catharsis/features/entity/conditions/BabyEntityCondition.kt
@@ -3,22 +3,13 @@ package me.owdding.catharsis.features.entity.conditions
 import com.mojang.serialization.MapCodec
 import me.owdding.catharsis.generated.CatharsisCodecs
 import me.owdding.ktcodecs.GenerateCodec
-import net.minecraft.world.entity.AgeableMob
 import net.minecraft.world.entity.Entity
-import net.minecraft.world.entity.monster.Zoglin
-import net.minecraft.world.entity.monster.piglin.Piglin
-import net.minecraft.world.entity.monster.zombie.Zombie
+import net.minecraft.world.entity.LivingEntity
 
 @GenerateCodec
 data class BabyEntityCondition(val isBaby: Boolean) : EntityCondition {
 
     override val codec: MapCodec<out EntityCondition> = CatharsisCodecs.getMapCodec<BabyEntityCondition>()
 
-    override fun matches(entity: Entity) = when (entity) {
-        is AgeableMob -> isBaby
-        is Zombie -> isBaby
-        is Piglin -> isBaby
-        is Zoglin -> isBaby
-        else -> false
-    } == isBaby
+    override fun matches(entity: Entity) = (entity as? LivingEntity)?.isBaby == isBaby
 }

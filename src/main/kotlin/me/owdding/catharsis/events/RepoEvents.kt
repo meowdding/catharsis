3c1437a5eb04eda4d9aeb83f50bfacfd3646df59
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2026-05-17 23:26:35 +0200

    feat: max_health entity condition (#104)
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/me/owdding/catharsis/events/RepoEvents.kt
+++ src/main/kotlin/me/owdding/catharsis/events/RepoEvents.kt
@@ -1,6 +1,9 @@
 package me.owdding.catharsis.events
 
+import me.owdding.catharsis.repo.CatharsisRemoteRepo
 import tech.thatgravyboat.skyblockapi.api.events.base.SkyBlockEvent
 
 object StartRepoLoadEvent : SkyBlockEvent()
-object FinishRepoLoadEvent : SkyBlockEvent()
+object FinishRepoLoadEvent : SkyBlockEvent() {
+    fun getAsJson(file: String) = CatharsisRemoteRepo.getFileContentAsJson(file)
+}

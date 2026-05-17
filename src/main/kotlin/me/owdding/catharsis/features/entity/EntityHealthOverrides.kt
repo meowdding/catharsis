fe39f5203075a2ebe45f74db101d36a581dac086
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2026-05-17 17:42:37 +0200

    trying to fix shit and failing
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/me/owdding/catharsis/features/entity/EntityHealthOverrides.kt
+++ src/main/kotlin/me/owdding/catharsis/features/entity/EntityHealthOverrides.kt
@@ -10,7 +10,7 @@ import net.minecraft.world.entity.LivingEntity
 import net.minecraft.world.entity.ai.attributes.Attributes
 import tech.thatgravyboat.skyblockapi.api.data.MayorPerk
 import tech.thatgravyboat.skyblockapi.api.events.base.Subscription
-import tech.thatgravyboat.skyblockapi.utils.extentions.cleanName
+import tech.thatgravyboat.skyblockapi.api.location.SkyBlockIsland
 import tech.thatgravyboat.skyblockapi.utils.extentions.serverValue
 import tech.thatgravyboat.skyblockapi.utils.json.Json.toData
 
@@ -19,39 +19,35 @@ object EntityHealthOverrides {
 
     private var cache: HealthModifiers? = null
 
-    fun doesHealthMatch(entity: LivingEntity, range: FloatPredicate, useModifiers: Boolean): Boolean {
+    fun doesHealthMatch(entity: LivingEntity, predicate: FloatPredicate, useModifiers: Boolean): Boolean {
         val health = entity.getAttribute(Attributes.MAX_HEALTH)?.serverValue ?: return false
-        if (!useModifiers) return range.contains(health)
-        val modifiers = cache ?: return range.contains(health)
+        if (!useModifiers) return predicate.contains(health)
+        val modifiers = cache ?: return predicate.contains(health)
 
-        var totalMultiplier = 1.0f
+        if (predicate.contains(health)) return true
 
-        if (isRunic(entity)) {
-            totalMultiplier *= modifiers.runic
-        }
-
-        if (isHealthy(entity)) {
-            totalMultiplier *= modifiers.healthy
-        }
-
+        var mayorMultiplier = 1.0f
         modifiers.mayorPerks.forEach { (perk, multiplier) ->
             if (perk.active) {
-                totalMultiplier *= multiplier
+                mayorMultiplier *= multiplier
             }
         }
+        if (mayorMultiplier == 0.0f) return false
 
-        if (totalMultiplier == 0.0f) return false
+        if (predicate.contains(health / mayorMultiplier)) return true
 
-        return range.contains(health / totalMultiplier)
-    }
+        val runicMultiplier = mayorMultiplier * modifiers.runic
+        if (runicMultiplier != 0.0f && predicate.contains(health / runicMultiplier)) return true
 
-    // TODO: Create powerful SkyBlockMob in sbapi that has like mobtype etc
-    private fun isRunic(entity: LivingEntity): Boolean {
-        return entity.cleanName.contains("Runic")
-    }
+        if (SkyBlockIsland.THE_CATACOMBS.inIsland()) {
+            val healthyMultiplier = mayorMultiplier * modifiers.healthy
+            if (healthyMultiplier != 0.0f && predicate.contains(health / healthyMultiplier)) return true
 
-    private fun isHealthy(entity: LivingEntity): Boolean {
-        return entity.cleanName.contains("Healthy")
+            val bothMultiplier = runicMultiplier * modifiers.healthy
+            if (bothMultiplier != 0.0f && predicate.contains(health / bothMultiplier)) return true
+        }
+
+        return false
     }
 
     @Subscription

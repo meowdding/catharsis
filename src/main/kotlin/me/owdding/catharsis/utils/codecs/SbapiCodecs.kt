f201529c84d63b195bc80066eedeb9d803e7b9fe
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2026-05-17 22:15:56 +0200

    ?????????
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/me/owdding/catharsis/utils/codecs/SbapiCodecs.kt
+++ src/main/kotlin/me/owdding/catharsis/utils/codecs/SbapiCodecs.kt
@@ -2,6 +2,7 @@ package me.owdding.catharsis.utils.codecs
 
 import com.mojang.datafixers.util.Either
 import com.mojang.serialization.Codec
+import com.mojang.serialization.DataResult
 import me.owdding.catharsis.generated.EnumCodec
 import me.owdding.ktcodecs.IncludedCodec
 import tech.thatgravyboat.skyblockapi.api.data.MayorPerk
@@ -20,8 +21,8 @@ object SbapiCodecs {
     ).xmap({ Either.unwrap(it) }, { Either.left(it) })
 
     @IncludedCodec(keyable = true)
-    val mayorPerkCodec: Codec<MayorPerk> = Codec.STRING.xmap(
-        { id -> MayorPerks.getPerkById(id) },
+    val mayorPerkCodec: Codec<MayorPerk> = Codec.STRING.comapFlatMap<MayorPerk>(
+        { id -> MayorPerks.getPerkById(id)?.let { DataResult.success(it) } },
         { perk -> perk.id }
     )
 

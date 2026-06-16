868435b8d58eb43740041b6753c0a6840327a644
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2026-06-16 13:41:19 +0200

    fix: slot item model condition not working
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/me/owdding/catharsis/features/gui/definitions/slots/SlotCondition.kt
+++ src/main/kotlin/me/owdding/catharsis/features/gui/definitions/slots/SlotCondition.kt
@@ -38,7 +38,7 @@ object SlotConditions {
         ID_MAPPER.put(Catharsis.id("slot"), CatharsisCodecs.getMapCodec<SlotIndexCondition>())
         ID_MAPPER.put(Catharsis.id("id"), CatharsisCodecs.getMapCodec<SlotSkyBlockIdCondition>())
         ID_MAPPER.put(Catharsis.id("item"), CatharsisCodecs.getMapCodec<SlotItemCondition>())
-        ID_MAPPER.put(Catharsis.id("item_model"), CatharsisCodecs.getMapCodec<SlotItemCondition>())
+        ID_MAPPER.put(Catharsis.id("item_model"), CatharsisCodecs.getMapCodec<SlotItemModelCondition>())
         ID_MAPPER.put(Catharsis.id("name"), CatharsisCodecs.getMapCodec<SlotNameCondition>())
         ID_MAPPER.put(Catharsis.id("lore"), CatharsisCodecs.getMapCodec<SlotLoreCondition>())
         ID_MAPPER.put(Catharsis.id("has_component"), CatharsisCodecs.getMapCodec<HasComponentCondition>())

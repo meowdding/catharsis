cf982ef053305c94f4f104d2f392f047ed4a4db1
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2026-06-18 19:47:06 +0200

    refactor: center entity debug billboard
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/me/owdding/catharsis/features/entity/DebugEntityDefinition.kt
+++ src/main/kotlin/me/owdding/catharsis/features/entity/DebugEntityDefinition.kt
@@ -3,10 +3,10 @@ package me.owdding.catharsis.features.entity
 import me.owdding.catharsis.utils.debugToggle
 import me.owdding.ktmodules.Module
 import net.minecraft.gizmos.Gizmos
-import net.minecraft.util.ARGB
+import net.minecraft.gizmos.TextGizmo
+import net.minecraft.world.phys.Vec3
 import tech.thatgravyboat.skyblockapi.api.events.base.Subscription
 import tech.thatgravyboat.skyblockapi.api.events.render.RenderEntityEvent
-import tech.thatgravyboat.skyblockapi.utils.text.TextColor
 
 @Module
 object DebugEntityDefinition {
@@ -18,6 +18,8 @@ object DebugEntityDefinition {
         if (!debug) return
         val entity = event.entity ?: return
         val definition = CustomEntityDefinitions.getFor(entity) ?: return
-        Gizmos.billboardTextOverMob(event.entity, 2, "Entity Definition: $definition", ARGB.opaque(TextColor.WHITE), 0.32f)
+        val y = entity.y + 2.4 + 2 * 0.25
+        val style = TextGizmo.Style.whiteAndCentered().withScale(0.32f)
+        Gizmos.billboardText("Entity Definition: $definition", Vec3(entity.x, y, entity.z), style)
     }
 }

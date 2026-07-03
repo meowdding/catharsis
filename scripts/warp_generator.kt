7f6a93e5b824f33f0e679cbc2e4c3aa7c5937aea
June <jvne.hibiscus@gmail.com>
2026-07-03 02:03:24 +0200

    feat: update fast travel script

--- scripts/warp_generator.kt
+++ scripts/warp_generator.kt
@@ -3,11 +3,11 @@ import me.owdding.catharsis.features.gui.definitions.GuiDefinition
 import me.owdding.catharsis.features.gui.definitions.conditions.GuiDefinitionTitleCondition
 import me.owdding.catharsis.features.gui.definitions.slots.GuiSlotDefinition
 import me.owdding.catharsis.features.gui.definitions.slots.SlotAllCondition
-import me.owdding.catharsis.features.gui.definitions.slots.SlotAnyCondition
 import me.owdding.catharsis.features.gui.definitions.slots.SlotItemCondition
 import me.owdding.catharsis.features.gui.definitions.slots.SlotLoreCondition
 import me.owdding.catharsis.features.gui.definitions.slots.SlotNameCondition
 import me.owdding.catharsis.features.gui.definitions.slots.SlotNotCondition
+import me.owdding.catharsis.features.gui.definitions.slots.SlotIndexCondition
 import me.owdding.catharsis.features.gui.matchers.EqualsTextMatcher
 import me.owdding.catharsis.features.gui.matchers.RegexTextMatcher
 import me.owdding.catharsis.features.gui.matchers.TextMatcher
@@ -17,25 +17,46 @@ import kotlin.io.path.Path
 import kotlin.io.path.createParentDirectories
 import kotlin.io.path.writeText
 
-fun warp(name: String, island: String, id: String) = listOf(
-    GuiSlotDefinition(
-        of("skyblock_gui", "fast_travel/$island/${id}_locked"),
-        SlotAllCondition(
-            SlotNameCondition(EqualsTextMatcher(name)),
-            SlotAnyCondition(
-                SlotItemCondition(Items.BEDROCK),
-                SlotLoreCondition(EqualsTextMatcher("Warp not unlocked!"), Either.left(-1)),
+val mainMenuSlotIndexes = mapOf(
+    "dynamic/spawn" to SlotIndexCondition(10),
+    "hub/spawn" to SlotIndexCondition(11),
+    "dungeon_hub/spawn" to SlotIndexCondition(12),
+    "farming_1/spawn" to SlotIndexCondition(13),
+    "foraging_1/spawn" to SlotIndexCondition(14),
+    "foraging_2/spawn" to SlotIndexCondition(15),
+    "mining_1/spawn" to SlotIndexCondition(16),
+    "mining_2/spawn" to SlotIndexCondition(19),
+    "mining_3/spawn" to SlotIndexCondition(20),
+    "crystal_hollows/entrance" to SlotIndexCondition(21),
+    "combat_1/spawn" to SlotIndexCondition(22),
+    "combat_3/spawn" to SlotIndexCondition(23),
+    "crimson_isle/spawn" to SlotIndexCondition(24),
+    "garden/spawn" to SlotIndexCondition(25),
+    "rift/wizard_tower" to SlotIndexCondition(29),
+    "fishing_1/spawn" to SlotIndexCondition(30),
+    "lotus_atoll/spawn" to SlotIndexCondition(32),
+    "winter/spawn" to SlotIndexCondition(33)
+)
+
+fun warp(name: String, island: String, id: String): List<GuiSlotDefinition> {
+    val unlockedCondition = SlotLoreCondition(EqualsTextMatcher("Warp not unlocked!"), Either.left(-1))
+    return listOf(
+        GuiSlotDefinition(
+            of("skyblock_gui", "fast_travel/$island/${id}_locked"),
+            SlotAllCondition(
+                SlotNameCondition(EqualsTextMatcher(name)),
+                unlockedCondition,
             ),
         ),
-    ),
-    GuiSlotDefinition(
-        of("skyblock_gui", "fast_travel/$island/${id}_unlocked"),
-        SlotAllCondition(
-            SlotNameCondition(EqualsTextMatcher(name)),
-            SlotNotCondition(SlotItemCondition(Items.BEDROCK)),
+        GuiSlotDefinition(
+            of("skyblock_gui", "fast_travel/$island/${id}_unlocked"),
+            SlotAllCondition(
+                SlotNameCondition(EqualsTextMatcher(name)),
+                SlotNotCondition(unlockedCondition),
+            ),
         ),
-    ),
-)
+    )
+}
 
 fun warpMenu(
     title: String,
@@ -68,24 +89,7 @@ fun mainMenu(
                     SlotNameCondition(EqualsTextMatcher("Island Browser")),
                 ),
             )
-            add(
-                GuiSlotDefinition(
-                    of("skyblock_gui", "fast_travel/paper_icons/enabled"),
-                    SlotAllCondition(
-                        SlotNameCondition(EqualsTextMatcher("Paper Icons")),
-                        SlotItemCondition(Items.FILLED_MAP),
-                    ),
-                ),
-            )
-            add(
-                GuiSlotDefinition(
-                    of("skyblock_gui", "fast_travel/paper_icons/disabled"),
-                    SlotAllCondition(
-                        SlotNameCondition(EqualsTextMatcher("Paper Icons")),
-                        SlotItemCondition(Items.MAP),
-                    ),
-                ),
-            )
+
             add(
                 GuiSlotDefinition(
                     of("skyblock_gui", "fast_travel/advanced_mode/enabled"),
@@ -95,6 +99,7 @@ fun mainMenu(
                     ),
                 ),
             )
+
             add(
                 GuiSlotDefinition(
                     of("skyblock_gui", "fast_travel/advanced_mode/disabled"),
@@ -104,17 +109,21 @@ fun mainMenu(
                     ),
                 ),
             )
-            add(
-                GuiSlotDefinition(
-                    of("skyblock_gui", "fast_travel/undiscovered_island"),
-                    SlotNameCondition(EqualsTextMatcher("Undiscovered Island")),
-                ),
-            )
 
             for ((id, name) in entries) {
                 add(
                     GuiSlotDefinition(
-                        of("skyblock_gui", "fast_travel/${id}_unlocked"),
+                        of("skyblock_gui", "fast_travel/${id}/undiscovered"),
+                        SlotAllCondition(
+                            SlotNameCondition(EqualsTextMatcher("Undiscovered Island")),
+                            mainMenuSlotIndexes [id]?: error("meow $id"),
+                    ),
+                    ),
+                )
+
+                add(
+                    GuiSlotDefinition(
+                        of("skyblock_gui", "fast_travel/${id}/unlocked"),
                         SlotNameCondition(name),
                     ),
                 )
@@ -129,39 +138,57 @@ fun mainMenu(
 
 fun warps() {
     mainMenu(
-        "hub/spawn" to EqualsTextMatcher("SkyBlock Hub"),
-        "private_island/spawn" to EqualsTextMatcher("Private Island"),
+        "hub/spawn" to RegexTextMatcher("Sky[bB]lock Hub"),
+        "dynamic/spawn" to EqualsTextMatcher("Private Island"),
         "dungeon_hub/spawn" to RegexTextMatcher("Dungeon Hub(?: - Spawn)?"),
-        "farming_islands/spawn" to RegexTextMatcher("The Barn(?: - Spawn)?"),
-        "park/spawn" to RegexTextMatcher("The Park(?: - Spawn)?"),
-        "galatea/spawn" to RegexTextMatcher("Galatea(?: - Spawn)?"),
-        "gold_mine/spawn" to RegexTextMatcher("Gold Mine(?: - Spawn)?"),
-        "deep_caverns/spawn" to RegexTextMatcher("Deep Caverns(?: - Spawn)?"),
-        "dwarven_mines/spawn" to RegexTextMatcher("Dwarven Mines(?: - Spawn)?"),
+        "farming_1/spawn" to RegexTextMatcher("The Barn(?: - Spawn)?"),
+        "foraging_1/spawn" to RegexTextMatcher("The Park(?: - Spawn)?"),
+        "foraging_2/spawn" to RegexTextMatcher("Galatea(?: - Spawn)?"),
+        "mining_1/spawn" to RegexTextMatcher("Gold Mine(?: - Spawn)?"),
+        "mining_2/spawn" to RegexTextMatcher("Deep Caverns(?: - Spawn)?"),
+        "mining_3/spawn" to RegexTextMatcher("Dwarven Mines(?: - Spawn)?"),
         "crystal_hollows/entrance" to RegexTextMatcher("Crystal Hollows(?: - Entrance)?"),
-        "spiders_den/spawn" to RegexTextMatcher("Spider's Den(?: - Spawn)?"),
-        "the_end/spawn" to RegexTextMatcher("The End(?: - Spawn)?"),
+        "combat_1/spawn" to RegexTextMatcher("Spider's Den(?: - Spawn)?"),
+        "combat_3/spawn" to RegexTextMatcher("The End(?: - Spawn)?"),
         "crimson_isle/spawn" to RegexTextMatcher("Crimson Isle(?: - Spawn)?"),
         "garden/spawn" to EqualsTextMatcher("The Garden"),
         "rift/wizard_tower" to RegexTextMatcher("The Rift(?: - Wizard Tower)?"),
-        "backwater_bayou/spawn" to RegexTextMatcher("Backwater Bayou(?: - Spawn)?"),
+        "fishing_1/spawn" to RegexTextMatcher("Backwater Bayou(?: - Spawn)?"),
         "lotus_atoll/spawn" to RegexTextMatcher("Lotus Atoll(?: - Spawn)?"),
-        "jerrys_workshop/spawn" to EqualsTextMatcher("Warp to: Jerry's Workshop"),
+        "winter/spawn" to EqualsTextMatcher("Warp to: Jerry's Workshop"),
+    )
+
+    warpMenu(
+        "Hub Warps",
+        "hub",
+        listOf(
+            "SkyBlock Hub" to "spawn",
+            "Hub - Castle" to "castle",
+            "SkyBlock Hub - Elizabeth" to "elizabeth",
+            "SkyBlock Hub - Sirius' Shack" to "sirius",
+            "SkyBlock Hub - Crypts" to "crypts",
+            "SkyBlock Hub - Museum" to "museum",
+            "SkyBlock Hub - Taylor's Shop" to "taylor",
+            "SkyBlock Hub - Wizard Tower" to "wizard",
+            "SkyBlock Hub - Carnival" to "carnival",
+            "SkyBlock Hub - Trading Center" to "trading",
+        ),
     )
 
     warpMenu(
         "The Farming Islands Warps",
-        "farming_islands",
+        "farming_1",
         listOf(
             "The Barn - Spawn" to "spawn",
             "Mushroom Desert - Spawn" to "desert",
-            "Mushroom Desert - Trapper's Den" to "trapper",
+            "Mushroom Desert - Trapper's Den" to "trappers",
+            "Mushroom Desert - Glowing Mushroom Cave" to "glowing_mushroom_cave",
         ),
     )
 
     warpMenu(
         "The Park Warps",
-        "the_park",
+        "foraging_1",
         listOf(
             "The Park - Spawn" to "spawn",
             "The Park - Jungle" to "jungle",
@@ -171,7 +198,7 @@ fun warps() {
 
     warpMenu(
         "Galatea Warps",
-        "galatea",
+        "foraging_2",
         listOf(
             "Galatea - Spawn" to "spawn",
             "Galatea - Murkwater Loch" to "murkwater",
@@ -180,7 +207,7 @@ fun warps() {
 
     warpMenu(
         "Dwarven Mines Warps",
-        "dwarven_mines",
+        "mining_3",
         listOf(
             "Dwarven Mines - Spawn" to "spawn",
             "Dwarven Mines - Forge" to "forge",
@@ -199,7 +226,7 @@ fun warps() {
 
     warpMenu(
         "Spider's Den Warps",
-        "spiders_den",
+        "combat_1",
         listOf(
             "Spider's Den - Spawn" to "spawn",
             "Spider's Den - Top of Nest" to "top_of_nest",
@@ -209,7 +236,7 @@ fun warps() {
 
     warpMenu(
         "The End Warps",
-        "the_end",
+        "combat_3",
         listOf(
             "The End - Spawn" to "spawn",
             "The End - Dragon's Nest" to "nest",
